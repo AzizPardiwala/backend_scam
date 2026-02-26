a439dda28e191c2fdeadfedd4f52be9f3e72750a
Aziz Pardiwala <azizpardiwala@Azizs-MacBook-Air.local>
2026-02-27 04:33:34 +0530

    Updated created_at to use PostgreSQL server default

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -13,3 +14,11 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
