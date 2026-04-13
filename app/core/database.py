6b7c6e5e14caf54c3935e2b6545294cc14929d95
Aziz Pardiwala <azizpardiwala@Azizs-MacBook-Air.local>
2026-04-13 13:54:18 +0530

    final scam backend

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# ✅ Use SQLite for now (works on Render easily)
 DATABASE_URL = "sqlite:///./scam.db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
